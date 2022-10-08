8f8a7ff57f50507872fcce9465ff84c9b76cf4d3
Vaibhav Tiwari <107205805+Vaibhav882@users.noreply.github.com>
2022-10-08 04:53:39 -0900

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,10 @@
+  //calculate length of string using printf()
+  #include<stdio.h>
+  int main()
+  {
+    int x;
+   x = printf("VAIBHAV");
+    printf("lenghth of string is %d",x);
+    return 0;
+  }
+  
